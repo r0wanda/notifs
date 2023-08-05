75ed963d6558ded634eb305d3a5967170499d201
cookey-dev <84088833+cookey-dev@users.noreply.github.com>
2023-08-05 20:25:42 +0000

    Initial commit

---
+++ notifs.js
@@ -0,0 +1,11 @@
+class Notifs {
+	boxes;
+	constructor() {
+		this.boxes = [];
+		this.#loadCSS();
+	}
+	#loadCSS() {
+		if (!document.querySelector('link[data-ckdev="notif-css"]'))
+	}
+	add(type='info')
+}
