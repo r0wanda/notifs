92bd20bf358178edbac7ab534f39d7c1c8f3c5c7
cookey-dev <84088833+cookey-dev@users.noreply.github.com>
2023-08-06 02:05:39 +0000

    final version

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Notifications
+
+A tiny on-page (not push) notification/toast engine
+
+### IMPORTANT: For icons to work, [Font Awesome](https://fontawesome.com/) must be included
+
+## Installation
+The CSS will be loaded automatically when a instance is created
+```html
+<script src="https://cdn.jsdelivr.net/gh/cookey-dev/notifs@main/notifs.js"></script>
+```
+## Usage
+```javascript
+var notifs = new Notifs();
+
+// Note: notifs.log may be used instead of info
+notifs.info('Info'); // Sends an info notification, disappears after awhile
+
+notifs.warn('Warning'); // Same as info, but styled to indicate a warning
+
+notifs.error('Error!'); // Sends an error notification, will not disappear unless manually dismissed
+```
--- notifs.js
+++ notifs.js
@@ -1,13 +1,94 @@
 class Notifs {
 	boxes;
-	constructor() {
+	#ids;
+	constructor(css='https://cdn.jsdelivr.net/gh/cookey-dev/notifs@main/notifs.css') {
 		this.boxes = [];
-		this.#loadCSS();
+		this.#ids = [];
+		this.#loadCSS(css);
+		window.addEventListener('resize', () => {
+			setTimeout(() => { this.resetPositions(); }, 1100);
+		});
 	}
-	#loadCSS() {
-		if (!document.querySelector('link[data-ckdev="notif-css"]')) {
-			
+	#getId() {
+		var id;
+		do {
+			id = (Math.random() + 1).toString(36).substring(7);
+		} while (this.#ids.includes(id));
+		this.#ids.push(id);
+		return id;
+	}
+	boxesHeight(idx=null) {
+		var h = 0;
+		for (var i = 0; i < this.boxes.length; i++) {
+			if (idx !== null && i == idx) break;
+			h += this.boxes[i].clientHeight + 20;
+		}
+		return h;
+	}
+	cleanBoxes() {
+		this.boxes = this.boxes.filter(b => document.querySelector(`div.ckdev-notif[data-id="${b.dataset.id}"]`));
+	}
+	resetPositions() {
+		for (var i = 0; i < this.boxes.length; i++) {
+			var n = this.boxes[i];
+			n.style.top = `${this.boxesHeight(i)}px`;
 		}
 	}
-	add(type='info')
+	#loadCSS(css) {
+		if (!document.querySelector('link[data-ckdev="notif-css"]')) {
+			const head = document.getElementsByTagName('head')[0];
+			if (!head) alert('CRITICAL: `head` element not found (notifs)');
+			const link = document.createElement('link');
+			link.rel = 'stylesheet';
+			link.href = css;
+			link.type = 'text/css';
+			link.dataset.ckdev = 'notif-css';
+			head.appendChild(link);
+		}
+	}
+	send(msg, type='info', fa='info', timeout=true) {
+		var notif = document.createElement('div');
+		notif.className = `ckdev-notif ckdev-${type}`;
+		const id = this.#getId();
+		notif.dataset.id = id;
+		if (this.boxes.length > 0) notif.style.top = `${this.boxesHeight()}px`;
+		var notifsClass = this;
+		function rem() {
+			this.style.animation = 'ckdev-notif-fade-out .5s linear forwards';
+			setTimeout(() => {
+				this.remove();
+				notifsClass.boxes.splice(notifsClass.boxes.indexOf(this), 1);
+				notifsClass.resetPositions();
+			}, 1000);
+		}
+		notif.addEventListener('click', function() {
+			rem.bind(this)();
+		});
+		console.log(notif);
+		const faElem = document.createElement('i');
+		faElem.className = `fa-solid fa-${fa}`;
+		const msgElem = document.createElement('span');
+		msgElem.innerText = msg;
+		notif.appendChild(faElem);
+		notif.appendChild(msgElem);
+		document.body.appendChild(notif);
+		notif = document.querySelector(`div.ckdev-notif[data-id="${id}"]`);
+		this.boxes.push(notif);
+		this.resetPositions();
+		if (timeout != false) setTimeout(() => {
+			rem.bind(notif)();
+		}, timeout === true ? msg.split(/\s/).length * 1000 + 5000 : parseInt(timeout));
+	}
+	info(msg) {
+		this.send(msg);
+	}
+	log(msg) {
+		this.send(msg);
+	}
+	warn(msg) {
+		this.send(msg, 'warning', 'triangle-exclamation');
+	}
+	error(msg) {
+		this.send(msg, 'error', 'circle-exclamation', false);
+	}
 }
